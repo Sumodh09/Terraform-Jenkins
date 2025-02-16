70ec79047d6cdb8ea0c8dfc3a52de0d3feeb291c
Sumodh c Sabu <52120967+Sumodh09@users.noreply.github.com>
2025-02-16 11:24:05 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_cloudformation_stack" "child_cft" {
+  name         = "Child-CFT-Deployment"
+  template_body = file("../Child-CFT/demo-cft.yaml")
+
+  capabilities = ["CAPABILITY_NAMED_IAM"]
+}

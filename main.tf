f0870073bfee458c7f6614316daeb548203a5075
Sumodh c Sabu <52120967+Sumodh09@users.noreply.github.com>
2025-02-17 17:43:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA2HEQVMFDAYZEM63W"
-  secret_key = "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"
 }
+
 
 resource "aws_cloudformation_stack" "child_cft" {
   name         = "Child-CFT-Deployment"

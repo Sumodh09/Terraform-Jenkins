b717e663f29c2d612be0661767fecb2927cd175e
Sumodh c Sabu <52120967+Sumodh09@users.noreply.github.com>
2025-02-17 17:42:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA2HEQVMFDAYZEM63W"
+  secret_key = "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"
+}
+
 resource "aws_cloudformation_stack" "child_cft" {
   name         = "Child-CFT-Deployment"
   template_body = file("Child-CFT/demo-cft.yaml")

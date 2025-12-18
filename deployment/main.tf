7fc13a8414d1f3ee0be893e2492132d2c6c88dd9
Sumodh c Sabu <52120967+Sumodh09@users.noreply.github.com>
2025-12-18 11:52:05 +0530

    Update main.tf

--- deployment/main.tf
+++ deployment/main.tf
@@ -49,7 +49,7 @@ resource "aws_s3_object" "my_script_zip_name" {
   #key    = "index.zip" 
   #source = "${path.module}/Child-Script/index.zip"
   key    = "Rule_Name_Check_Script.zip" 
-  source = "${path,module}/Child-Script/Rule_Name_Check_Script.zip"
+  source = "${path.module}/Child-Script/Rule_Name_Check_Script.zip"
   acl    = "private"
 
   depends_on = [null_resource.zip_python_script_name]
